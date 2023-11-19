54883688d80b14ab33c03f085aa5df8948ee81b6
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-11-19 08:46:44 -0800

    Begin refactoring with async function

--- script.js
+++ script.js
@@ -12,16 +12,9 @@ function getImage(input) {
   console.log(`URL = ${URL}`);
   fetch(URL, { mode: "cors" })
     .then(function (response) {
-      // A Promise!
       return response.json();
     })
     .then(function (response) {
-      // Unwrapping another level of promise
-      console.log(response);
-      // Then getting the URL out of the object we have just returned^
-      console.log(response.data.images.original.url);
-      // Got it! Now let's set it as the image source:
-
       let reponseData = response.data;
       if (reponseData == []) {
         console.log(`edge case error achieved?`);
@@ -41,6 +34,29 @@ search.addEventListener("click", () => {
 });
 
 getImage(`dogs`);
+
+
+// written using an async function:
+
+async function getGiph(input) {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=1dYtDfVcAbsOe6Ipxa6Rc6cgtMy8tuk4&s=${input}`, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response){
+        img.src = response.data.images.original.url;
+    })
+}   
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -69,10 +85,6 @@ const server = {
       });
     },
   };
-
-
-
-
 
   function getPersonsInfo(name) {
     return server.getPeople().then(people => {
