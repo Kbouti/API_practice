57a9fe6fde36ed774f34ed65f5ef6de9e1d92cd1
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-11-19 14:31:36 -0800

    Complete first async/await exercice

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// rockshox thru shaft service:
-// https://www.youtube.com/watch?v=HjDDTVyV3DU
-
-
+// Our first API request
 
 const img = document.querySelector(`img`);
 const search = document.getElementById("search");
@@ -35,62 +32,87 @@ search.addEventListener("click", () => {
 
 getImage(`dogs`);
 
-
 // written using an async function:
-// This is does the same thing as our original function above. Only difference is we haven't added in edge case hamdlers. 
+// This is does the same thing as our original function above. Only difference is we haven't added in edge case hamdlers.
 
 async function getGiph(input) {
-    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=1dYtDfVcAbsOe6Ipxa6Rc6cgtMy8tuk4&s=${input}`, {mode: 'cors'})
-    const apiData = await response.json();
-    img.src = apiData.data.images.original.url;
-}   
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=1dYtDfVcAbsOe6Ipxa6Rc6cgtMy8tuk4&s=${input}`,
+    { mode: "cors" }
+  );
+  const apiData = await response.json();
+  img.src = apiData.data.images.original.url;
+}
 
 getGiph(`trains`);
 
-
-
-
-
-
-
-
-
-
+// ************************************************************************************************
 
 const server = {
-    people: [
-      {
-        name: "Odin",
-        age: 20,
-      },
-      {
-        name: "Thor",
-        age: 35,
-      },
-      {
-        name: "Freyja",
-        age: 29,
-      },
-    ],
-  
-    getPeople() {
-      return new Promise((resolve, reject) => {
-        // Simulating a delayed network call to the server
-        setTimeout(() => {
-          resolve(this.people);
-        }, 2000);
-      });
+  people: [
+    {
+      name: "Odin",
+      age: 20,
     },
-  };
+    {
+      name: "Thor",
+      age: 35,
+    },
+    {
+      name: "Freyja",
+      age: 29,
+    },
+  ],
 
-  function getPersonsInfo(name) {
-    return server.getPeople().then(people => {
-        return people.find(person => { return person.name === name });
+  getPeople() {
+    return new Promise((resolve, reject) => {
+      // Simulating a delayed network call to the server
+      setTimeout(() => {
+        resolve(this.people);
+      }, 2000);
     });
-  }
+  },
+};
 
-  async function getPersonsInfo(name) {
-    const people = await server.getPeople();
-    const person = people.find(person => { return person.name === name });
-    return person;
+function getPersonsInfo(name) {
+  return server.getPeople().then((people) => {
+    return people.find((person) => {
+      return person.name === name;
+    });
+  });
+}
+
+async function getPersonsInfo(name) {
+  const people = await server.getPeople();
+  const person = people.find((person) => {
+    return person.name === name;
+  });
+  return person;
+}
+
+// ************************************************************************************************
+// exercise: Rewrite the following function with async/await instead of .then/catch:
+
+//   function loadJson(url) {
+//     return fetch(url)
+//       .then(response => {
+//         if (response.status == 200) {
+//           return response.json();
+//         } else {
+//           throw new Error(response.status);
+//         }
+//       });
+//   }
+
+// My attempt:
+
+async function loadJson(url) {
+  let response = await fetch(url);
+  if (response.status == 200) {
+    let json = await response.json();
+    return json;
   }
+  throw new Error(response.status);
+}
+
+loadJson("https://javascript.info/no-such-user.json").catch(alert); // Error: 404
