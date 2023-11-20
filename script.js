23f8db6040fecb4ce5945dfd6cbce5a67f48e46f
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-11-20 11:21:12 -0800

    Add next exercise

--- script.js
+++ script.js
@@ -203,3 +203,22 @@ async function demoGithubUser() {
 }
 
 demoGithubUser();
+
+// ************************************************************************************************
+// Next question:
+// We have a “regular” function called f. How can you call the async function wait() and use its result inside of f?
+
+async function wait() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  
+    return 10;
+  }
+  
+  function f() {
+    // ...what should you write here?
+    // we need to call async wait() and wait to get 10
+    // remember, we can't use "await"
+    //The answer:
+    wait().then(result => alert(result));
+    //I'm, not sure I really get this one.....
+  }
